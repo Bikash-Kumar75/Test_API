7290af14ec84a5c494f0b75dcf50b02a49876304
Bikash-Kumar75 <bikashkupradhan77@gmail.com>
2025-07-11 15:28:39 +0530

    Update JSON data

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ const data = [
         system_id: 35,
         name: "Design Lab",
         code_name: "ahmedabad-a",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-a/2025-07-09/ahmedabad-a-1752046703_09072025_073823.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-a/2025-07-09/ahmedabad-a-1752046703_09072025_073823.mp4",
         status: "active"
     },
@@ -10,6 +11,7 @@ const data = [
         system_id: 36,
         name: "Karnavati Hangar",
         code_name: "ahmedabad-b",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-b/2025-07-09/ahmedabad-b-1752038739_09072025_052539.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-b/2025-07-09/ahmedabad-b-1752038739_09072025_052539.mp4",
         status: "active"
     },
@@ -17,6 +19,7 @@ const data = [
         system_id: 37,
         name: "T2 Substation Area",
         code_name: "ahmedabad-c",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-c/2025-07-09/ahmedabad-c-1752043976_09072025_065256.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-c/2025-07-09/ahmedabad-c-1752043976_09072025_065256.mp4",
         status: "inactive"
     },
@@ -24,6 +27,7 @@ const data = [
         system_id: 38,
         name: "Fuel Farm",
         code_name: "ahmedabad-x",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-x/2025-07-09/ahmedabad-x-1752039565_09072025_053925.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-x/2025-07-09/ahmedabad-x-1752039565_09072025_053925.mp4",
         status: "active"
     },
@@ -31,6 +35,7 @@ const data = [
         system_id: 39,
         name: "JCPL Plant",
         code_name: "ahmedabad-y",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-y/2025-07-09/ahmedabad-y-1752042235_09072025_062355.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-y/2025-07-09/ahmedabad-y-1752042235_09072025_062355.mp4",
         status: "active"
     },
@@ -38,6 +43,7 @@ const data = [
         system_id: 75,
         name: "CSD - West Side",
         code_name: "ahmedabad-s",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-s/2025-07-09/ahmedabad-s-1752044648_09072025_070408.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-s/2025-07-09/ahmedabad-s-1752044648_09072025_070408.mp4",
         status: "active"
     },
@@ -45,6 +51,7 @@ const data = [
         system_id: 76,
         name: "CISF Barrack",
         code_name: "ahmedabad-t",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-t/2025-07-09/ahmedabad-t-1752043537_09072025_064537.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-t/2025-07-09/ahmedabad-t-1752043537_09072025_064537.mp4",
         status: "active"
     },
@@ -52,6 +59,7 @@ const data = [
         system_id: 77,
         name: "MRO",
         code_name: "ahmedabad-v",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-v/2025-07-09/ahmedabad-v-1752043077_09072025_063757.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-v/2025-07-09/ahmedabad-v-1752043077_09072025_063757.mp4",
         status: "active"
     },
@@ -59,6 +67,7 @@ const data = [
         system_id: 78,
         name: "CSD - East Side",
         code_name: "ahmedabad-u",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-u/2025-07-09/ahmedabad-u-1752044734_09072025_070534.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-u/2025-07-09/ahmedabad-u-1752044734_09072025_070534.mp4",
         status: "active"
     },
@@ -66,6 +75,7 @@ const data = [
         system_id: 79,
         name: "Ahmedabad-w",
         code_name: "ahmedabad-w",
+        thumbnail: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-w/2025-07-09/ahmedabad-w-1752044968_09072025_070928.mp4",
         url: "https://smile-ai-test.s3.ap-south-1.amazonaws.com/video-archive/ahmedabad-w/2025-07-09/ahmedabad-w-1752044968_09072025_070928.mp4",
         status: "active"
     }
